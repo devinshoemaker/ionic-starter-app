707d475772af21286246ea064546fbe87340e4fa
Devin Shoemaker <devinshoe@gmail.com>
2019-06-23 14:14:29 -0500

    HomePage: update title to "Ionic Starter App"

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,6 +7,11 @@ describe('new App', () => {
     page = new AppPage();
   });
 
+  it('should have title', () => {
+    page.navigateTo();
+    expect(page.getTitle()).toContain('Ionic Starter App');
+  });
+
   it('should be blank', () => {
     page.navigateTo();
     expect(page.getParagraphText()).toContain('The world is your oyster.');
